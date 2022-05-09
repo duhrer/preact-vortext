892c1ec6200b452b871be212d0af69eb5854a109
Tony Atkins <tony@raisingthefloor.org>
2022-05-09 14:55:52 +0200

    Cleaned up demo to publish on GH Pages.

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,8 @@
-import { h } from 'preact';
-import { Router } from 'preact-router';
-
-import Header from './header';
-
-// Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
-import Vortex from '../routes/vortex';
+import Vortex from './vortex';
 
 const App = () => (
 	<div id="app">
-		<Header />
-		<Router>
-			<Home path="/" />
-			<Vortex path="/vortex" />
-		</Router>
+		<Vortex />
 	</div>
 )
 
